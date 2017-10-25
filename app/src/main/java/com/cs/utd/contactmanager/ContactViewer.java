b69c396f470df898cf2b1a2986b64a2841ea3574
Marctif <Marc.tifrea@gmail.com>
2017-10-25 15:25:49 -0500

    Save support now works

--- app/src/main/java/com/cs/utd/contactmanager/ContactViewer.java
+++ app/src/main/java/com/cs/utd/contactmanager/ContactViewer.java
@@ -32,10 +32,12 @@ public class ContactViewer extends AppCompatActivity {
     FileManager fileManager;
 
     private static Context context;
+    public static final String PASS_ID = "com.example.myfirstapp.MESSAGE_ID";
     public static final String PASS_FIRST = "com.example.myfirstapp.MESSAGE_FIRST";
     public static final String PASS_LAST = "com.example.myfirstapp.MESSAGE_LAST";
     public static final String PASS_PHONE = "com.example.myfirstapp.MESSAGE_PHONE";
     public static final String PASS_EMAIL = "com.example.myfirstapp.MESSAGE_EMAIL";
+    public static final String PASS_EXISTING = "com.example.myfirstapp.MESSAGE_EXISTING";
 
     public static final int CONTACT_REQUEST = 1;
 
@@ -60,6 +62,53 @@ public class ContactViewer extends AppCompatActivity {
         fileManager = new FileManager();
         contactList = fileManager.readFile();
 
+        remakeListView();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+
+                String id = data.getStringExtra(ContactViewer.PASS_ID);
+                String first=data.getStringExtra(ContactViewer.PASS_FIRST);
+                String last=data.getStringExtra(ContactViewer.PASS_LAST);
+                String phone=data.getStringExtra(ContactViewer.PASS_PHONE);
+                String email=data.getStringExtra(ContactViewer.PASS_EMAIL);
+                boolean existing=data.getBooleanExtra(ContactViewer.PASS_EXISTING,true);
+
+                Contact contact = null;
+                if(existing){
+                    contact = findByID(id);
+                    contact.firstName = first;
+                    contact.lastName = last;
+                    contact.phoneNumber = phone;
+                    contact.email = email;
+                } else {
+                    contact = new Contact("" + findNextID(),first,last,phone,email);
+                }
+
+                remakeListView();
+
+                Toast toast = Toast.makeText(getApplicationContext(), contact.toString() , Toast.LENGTH_SHORT);
+
+                toast.show();
+
+                // Do something with the contact here (bigger example below)
+            }
+            if (resultCode == RESULT_CANCELED) {
+                Toast toast = Toast.makeText(getApplicationContext(), "SHOULD DELETE", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
+    public void remakeListView(){
         List<String> formattedList = toStringArray(contactList);
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
@@ -73,19 +122,15 @@ public class ContactViewer extends AppCompatActivity {
 
                 // selected item
                 String name = (String)parent.getAdapter().getItem(position);
-                String[] split = name.split(" ");
-                Contact match = null;
-                for(Contact c : contactList){
-                    if(c.firstName.equals( split[0]) && c.lastName.equals( split[1]) && c.phoneNumber.equals( split[2])){
-                        match = c;
-                    }
-                }
+                Contact match = findByName(name);
 
                 Intent intent = new Intent(context, EditContact.class);
+                intent.putExtra(PASS_ID,match.id);
                 intent.putExtra(PASS_FIRST, match.firstName);
                 intent.putExtra(PASS_LAST, match.lastName);
                 intent.putExtra(PASS_PHONE, match.phoneNumber);
                 intent.putExtra(PASS_EMAIL, match.email);
+                intent.putExtra(PASS_EXISTING, true);
                 startActivityForResult(intent, CONTACT_REQUEST);
 
                 /*Toast toast = Toast.makeText(getApplicationContext(), match.toString() + " " + match.email, Toast.LENGTH_SHORT);
@@ -93,25 +138,6 @@ public class ContactViewer extends AppCompatActivity {
                 */
             }
         });
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // Check which request we're responding to
-        if (requestCode == CONTACT_REQUEST) {
-            // Make sure the request was successful
-            if (resultCode == RESULT_OK) {
-                // The user picked a contact.
-                // The Intent's data Uri identifies which contact was selected.
-
-                String result=data.getStringExtra("result");
-                Toast toast = Toast.makeText(getApplicationContext(), "resut: " + result, Toast.LENGTH_SHORT);
-                toast.show();
-
-                // Do something with the contact here (bigger example below)
-            }
-        }
     }
 
     @Override
@@ -119,6 +145,16 @@ public class ContactViewer extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_contact_viewer, menu);
         return true;
+    }
+
+    public int findNextID(){
+        String max = "";
+        for(Contact c : contactList){
+            if(c.id.compareTo(max) > 0){
+                max = c.id;
+            }
+        }
+        return Integer.parseInt(max) + 1;
     }
 
     @Override
@@ -146,6 +182,27 @@ public class ContactViewer extends AppCompatActivity {
         return formattedList;
     }
 
+    public Contact findByName(String name){
+        String[] split = name.split(" ");
+        Contact match = null;
+        for(Contact c : contactList){
+            if(c.firstName.equals( split[0]) && c.lastName.equals( split[1]) && c.phoneNumber.equals( split[2])){
+                match = c;
+            }
+        }
+        return match;
+    }
+
+    public Contact findByID(String id){
+        Contact match = null;
+        for(Contact c : contactList){
+            if(c.id.equals(id)){
+                match = c;
+            }
+        }
+        return match;
+    }
+
     public class FileManager {
         String path;
 
@@ -170,7 +227,7 @@ public class ContactViewer extends AppCompatActivity {
                 List<Contact> contacts = new ArrayList<Contact>();
                 for(String s : fileLines){
                     String[] split = s.split(",");
-                    Contact contact = new Contact(split[0],split[1],split[2],split[3]);
+                    Contact contact = new Contact(split[0],split[1],split[2],split[3],split[4]);
                     contacts.add(contact);
                 }
 
@@ -188,7 +245,7 @@ public class ContactViewer extends AppCompatActivity {
                 AssetManager am = context.getAssets();
                 BufferedWriter updatedContactFile = new BufferedWriter(new FileWriter("contact.txt"));
                 for(Contact c : contactList){
-                    updatedContactFile.write(c.firstName + "," + c.lastName + "," + c.phoneNumber + "," + c.email + "\n");
+                    updatedContactFile.write(c.id + "," + c.firstName + "," + c.lastName + "," + c.phoneNumber + "," + c.email + "\n");
                 }
                 updatedContactFile.close();
             }
@@ -199,12 +256,14 @@ public class ContactViewer extends AppCompatActivity {
     }
 
     public class Contact {
+        String id;
         String firstName;
         String lastName;
         String phoneNumber;
         String email;
 
-        public Contact(String first,String last, String phoneNum, String email){
+        public Contact(String id,String first,String last, String phoneNum, String email){
+            this.id = id;
             firstName = first;
             lastName = last;
             phoneNumber = phoneNum;
--- app/src/main/java/com/cs/utd/contactmanager/EditContact.java
+++ app/src/main/java/com/cs/utd/contactmanager/EditContact.java
@@ -14,6 +14,8 @@ import android.widget.TextView;
 public class EditContact extends AppCompatActivity {
 
     private static Context context;
+    private static boolean existingContact;
+    private static String Contactid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +24,12 @@ public class EditContact extends AppCompatActivity {
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
+        Contactid = intent.getStringExtra(ContactViewer.PASS_ID);
         String firstName = intent.getStringExtra(ContactViewer.PASS_FIRST);
         String lastName = intent.getStringExtra(ContactViewer.PASS_LAST);
         String email = intent.getStringExtra(ContactViewer.PASS_EMAIL);
         String phone = intent.getStringExtra(ContactViewer.PASS_PHONE);
+        existingContact = intent.getBooleanExtra(ContactViewer.PASS_EXISTING,false);
 
         // Capture the layout's TextView and set the string as its text
         EditText textViewFirst = (EditText) findViewById(R.id.editContactFirst);
@@ -36,12 +40,17 @@ public class EditContact extends AppCompatActivity {
         textViewPhone.setText(phone);
         EditText textViewEmail = (EditText) findViewById(R.id.editContactEmail);
         textViewEmail.setText(email);
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_edit_contact, menu);
+        if(!existingContact)
+            menu.getItem(0).setVisible(false);
         return true;
     }
 
@@ -55,11 +64,56 @@ public class EditContact extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_save) {
             Intent returnIntent = new Intent();
-            String result = "MY RESULT";
-            returnIntent.putExtra("result",result);
+
+            // Capture the layout's TextView and get the string values
+            EditText textViewFirst = (EditText) findViewById(R.id.editContactFirst);
+            String firstName = textViewFirst.getText().toString();
+            EditText textViewLast = (EditText) findViewById(R.id.editContactLast);
+            String lastName = textViewLast.getText().toString();
+            EditText textViewPhone = (EditText) findViewById(R.id.editContactPhone);
+            String phone = textViewPhone.getText().toString();
+            EditText textViewEmail = (EditText) findViewById(R.id.editContactEmail);
+            String email = textViewEmail.getText().toString();
+
+            //Set the intent values
+            returnIntent.putExtra(ContactViewer.PASS_ID,Contactid);
+            returnIntent.putExtra(ContactViewer.PASS_FIRST, firstName);
+            returnIntent.putExtra(ContactViewer.PASS_LAST, lastName);
+            returnIntent.putExtra(ContactViewer.PASS_PHONE, phone);
+            returnIntent.putExtra(ContactViewer.PASS_EMAIL, email);
+            returnIntent.putExtra(ContactViewer.PASS_EXISTING, true);
+
+            //Return the intent
             setResult(Activity.RESULT_OK,returnIntent);
+
             finish();
         }
+        if (id == R.id.action_delete) {
+            Intent returnIntent = new Intent();
+
+            EditText textViewFirst = (EditText) findViewById(R.id.editContactFirst);
+            String firstName = textViewFirst.getText().toString();
+            EditText textViewLast = (EditText) findViewById(R.id.editContactLast);
+            String lastName = textViewLast.getText().toString();
+            EditText textViewPhone = (EditText) findViewById(R.id.editContactPhone);
+            String phone = textViewPhone.getText().toString();
+            EditText textViewEmail = (EditText) findViewById(R.id.editContactEmail);
+            String email = textViewEmail.getText().toString();
+
+            //Set the intent values
+            returnIntent.putExtra(ContactViewer.PASS_ID,Contactid);
+            returnIntent.putExtra(ContactViewer.PASS_FIRST, firstName);
+            returnIntent.putExtra(ContactViewer.PASS_LAST, lastName);
+            returnIntent.putExtra(ContactViewer.PASS_PHONE, phone);
+            returnIntent.putExtra(ContactViewer.PASS_EMAIL, email);
+            returnIntent.putExtra(ContactViewer.PASS_EXISTING, true);
+
+            //Return the intent
+            setResult(Activity.RESULT_CANCELED,returnIntent);
+
+            finish();
+        }
+
 
         return super.onOptionsItemSelected(item);
     }
